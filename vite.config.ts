fb2f73bb8fcfc5df8f62b423a2de526d32567dd0
Dinh Bang <ndinhbang111@gmail.com>
2023-04-23 17:40:43 +0700

    Adds subresource integrity hashes to script

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import sri from "vite-tiptop-sri"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    manifest: true,
+  },
+  plugins: [vue(), sri({ augmentManifest: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
