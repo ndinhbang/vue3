0f0b3326700929563dfbeb0e5a2e65d1fb250b02
Nguyen Dinh Bang <bangnd@abilive.vn>
2023-04-27 10:25:17 +0700

    customize less variables

--- vite.config.ts
+++ vite.config.ts
@@ -14,22 +14,20 @@ export default defineConfig({
             less: {
                 javascriptEnabled: true,
                 modifyVars: {
-                    'ant-prefix': 'b'
+                    'ant-prefix': 'b',
+                    'animation-duration-slow': '0.1s',
+                    'animation-duration-base': '0.1s',
                 },
             },
         },
     },
     plugins: [vue(), sri({augmentManifest: true, hashAlgorithms: ['sha256']})],
     resolve: {
-        alias: [
-            {
-                find: '@',
-                replacement: fileURLToPath(new URL('./src', import.meta.url))
-            },
-            // {
-            //     find: /^~/,
-            //     replacement: ''
-            // },
-        ]
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            // fix less import by: @import ~
+            // @see: https://github.com/vitejs/vite/issues/2185#issuecomment-784637827
+            // '/^~/' : '',
+        }
     }
 })
