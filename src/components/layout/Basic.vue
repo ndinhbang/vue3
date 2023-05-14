a7fc30742287f1e43859d53e29c4db6b2b697d0b
Dinh Bang <ndinhbang111@gmail.com>
2023-05-14 18:01:41 +0700

    install konsta ui

--- src/components/layout/Basic.vue
+++ src/components/layout/Basic.vue
@@ -1,22 +1,11 @@
 <script setup lang="ts">
-import Sidebar from "@/components/app/Sidebar.vue";
-import Header from "@/components/app/Header.vue";
 import {ref} from "vue";
-import Demo from "@/Demo.vue";
 
 const collapsed = ref<boolean>(false);
 </script>
 
 <template>
-    <a-layout has-sider class="app-layout">
-      <Sidebar />
-      <a-layout class="app-pages">
-        <Header />
-        <a-layout-content>
-          <Demo />
-        </a-layout-content>
-      </a-layout>
-    </a-layout>
+
 </template>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import './assets/css/index.css';
 import App from './App.vue'
 import router from './router'
-import DevUI from 'vue-devui';
-import 'vue-devui/style.css';
-import '@devui-design/icons/icomoon/devui-icon.css';
-import { ThemeServiceInit, Theme, devuiLightTheme } from 'devui-theme';
-
-const themeService = ThemeServiceInit();
-
-const myLightTheme: Theme = new Theme({
-    id: 'my-light-theme',
-    name: 'My Light Theme',
-    data: Object.assign({}, devuiLightTheme.data, {
-        'b-global-bg': '#ccc',
-    }),
-    isDark: false,
-});
-
-console.log(devuiLightTheme)
-
-themeService?.applyTheme(myLightTheme);
-
-// ThemeServiceInit({
-//     'my-light-theme': myLightTheme
-// }, 'my-light-theme');
-
-// import './assets/css/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(DevUI)
 
 app.mount('#app')
